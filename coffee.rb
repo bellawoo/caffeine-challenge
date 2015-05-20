c92231eaedb1f115882b13e26f255ad6533c4c99
Bella <woo.bella@gmail.com>
2015-05-20 10:48:20 -0400

    progress through figuring out wwhether humans need coffee

--- coffee.rb
+++ coffee.rb
@@ -1,3 +1,15 @@
 class Coffee
   # Add code here
+  def initialize type
+  	@sips = 3
+  end
+
+  def full?
+  	@sips == 3
+  end
+
+  def take_sip
+  	@sips -= 1
+  end
+
 end
--- human.rb
+++ human.rb
@@ -1,3 +1,33 @@
 class Human
   # Add code here
+  def initialize name
+  	@name = name
+  end
+
+  def alertness
+  	@alertness = alertness
+  end
+
+  def buy cup_coffee
+  	@coffee = coffee
+  end
+
+  def has_coffee?
+  	# !! @coffee --> returns boolean, which is concise way of saying the below, but below is more expressive
+  	if @coffee.nil?
+  		false
+  	else
+  		true
+  end
+
+  def needs_coffee?
+  	! has_coffee?
+  end
+
+  def drink!
+  	@alertness += 0.33
+  	@coffee.take_sip -= 1
+  end
+
 end
+
